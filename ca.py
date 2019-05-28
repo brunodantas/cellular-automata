bd2b3355e7d7d550b40fdbb654ba87f760139028
brunodantas <bruno.welldm@gmail.com>
2019-05-28 17:06:34 -0300

    fix function arguments

--- ca.py
+++ ca.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 # first implementation w/ string dict
 
-def make_rule(rule_number):
+def make_rule(rule_number, neighborhood_size):
     outputs = [int(n) for n in format(rule_number, "08b")]
     int_inputs = list(range(2**neighborhood_size))
     inputs = [format(y, "03b") for y in int_inputs]
@@ -15,7 +15,7 @@ def apply_rule(rule, values):
     v = "".join(str(x) for x in values)
     return rule[v]
 
-def make_generation(rule, previous_gen):
+def make_generation(rule, previous_gen, neighborhood_size):
     gen = []
     neighborhood = list(range(-(neighborhood_size//2), neighborhood_size//2 + 1))
     # print(neighborhood)
@@ -35,19 +35,14 @@ def render_ca(generations):
         for j in range(len(generations[0])):
             if generations[i][j] == 1:
                 pixels[j,i] = (0,0,0)
-    img.show()
+#     img.show()
+    from IPython.display import display # to display images
+    display(img)
 
-def run_ca(rule_number, inpt, steps):
-    rule = make_rule(rule_number)
+def run_ca(rule_number, inpt, steps, neighborhood_size):
+    rule = make_rule(rule_number, neighborhood_size)
     generations = [inpt]
     for i in range(steps):
         current = generations[-1]
-        generations.append(make_generation(rule, current))
-    render_ca(generations)
-
-
-steps = 100
-inpt = 100*[0] + [1] + 100*[0]
-neighborhood_size = 3
-rule_number = 90
-run_ca(rule_number, inpt, steps)
+        generations.append(make_generation(rule, current, neighborhood_size))
+    return generations
